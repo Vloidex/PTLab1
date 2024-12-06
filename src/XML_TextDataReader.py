b2e9c35888b2602346115854d122f93fe96c169e
Tsarenko Vladislav Alexandrovich <vloidex@gmail.com>
2024-12-06 13:52:00 +0300

    added an algorithm for reading students and grades to the class XML_TextDataReader

--- src/XML_TextDataReader.py
+++ src/XML_TextDataReader.py
@@ -12,4 +12,13 @@ class XML_TextDataReader(DataReader):
         tree = ElementTree.parse(path)
         root = tree.getroot()
 
+        for student in root:
+            fio = student.tag
+            score = []
+            for subject in student:
+                name_subject = subject.tag
+                score_subject = int(subject.text)
+                score.append(name_subject, score_subject)
+            self.students[fio] = score
+
         return self.students
