3e15fac17e2e3b86c34bd313d147c00d523a6d7e
Tsarenko Vladislav Alexandrovich <vloidex@gmail.com>
2024-12-07 15:24:37 +0300

    added algoritm in def  Quartile

--- src/QuartileRating.py
+++ src/QuartileRating.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
-from CalcRating import CalcRating
-
 RatingType = dict[str, float]
 
 
 class QuartileRating:
     def __init__(self, data: RatingType) -> None:
-        self.rating: RatingType = {}
+        self.rating: RatingType = data
 
     def Quartile(self) -> RatingType:
 
-        return self.rating
+        sorted_students_and_ratings = sorted(self.rating.items(), key=lambda x: x[1])
+        sorted_ratings = [rating for _, rating in sorted_students_and_ratings]
+        n = len(sorted_ratings)
+        Q1 = sorted_ratings[n // 4]
+        students_in_Q1 = {student: rating for student, rating in sorted_students_and_ratings if rating <= Q1}
+        
+        return students_in_Q1
