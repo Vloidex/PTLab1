55f9cd3f12c61123739dbf32ecff675b0b3c0e6e
Tsarenko Vladislav Alexandrovich <vloidex@gmail.com>
2024-12-07 18:11:54 +0300

    finished with filling out the TestQuartileRating class

--- test/test_QuartileRating.py
+++ test/test_QuartileRating.py
@@ -1,44 +1,35 @@
 # -*- coding: utf-8 -*-
-from src.Types import DataType
 from src.QuartileRating import QuartileRating
 import pytest
 
 RatingsType = dict[str, float]
 
 
-class QuartileRating:
+class TestQuartileRating:
     @pytest.fixture()
-    def input_data(self) -> tuple[DataType, RatingsType]:
-        data: DataType = {
-            "Абрамов Петр Сергеевич": [
-                ("математика", 80),
-                ("русский язык", 76),
-                ("программирование", 100),
-            ],
-            "Петров Игорь Владимирович": [
-                ("математика", 61),
-                ("русский язык", 80),
-                ("программирование", 78),
-                ("литература", 97),
-            ],
+    def input_data(self) -> tuple[RatingsType, RatingsType]:
+        data: RatingsType = {
+            "Абрамов Петр Сергеевич": 70.3333,
+            "Петров Игорь Владимирович": 94.0000,
+            "Фёдоров Дмитрий Николаевич": 80.0000,
+            "Попов Владимир Александрович": 75.0000,
+            "Смирнов Алексей Викторович": 61.5000
         }
 
-        rating_scores: RatingsType = {
-            "Абрамов Петр Сергеевич": 85.3333,
-            "Петров Игорь Владимирович": 79.0000,
+        students_Q1: RatingsType = {
+            "Смирнов Алексей Викторович": 61.5000,
+            "Абрамов Петр Сергеевич": 70.3333,
         }
 
-        return data, rating_scores
+        return data, students_Q1
 
-    def test_init_calc_rating(self, input_data: tuple[DataType,
+    def test_init_quartile_rating(self, input_data: tuple[RatingsType,
                                                       RatingsType]) -> None:
-        calc_rating = CalcRating(input_data[0])
-        assert input_data[0] == calc_rating.data
+        quartile_rating = QuartileRating(input_data[0])
+        assert input_data[0] == quartile_rating.data
 
-    def test_calc(self, input_data: tuple[DataType, RatingsType]) -> None:
+    def test_quartile(self, input_data: tuple[RatingsType, RatingsType]) -> None:
 
-        rating = CalcRating(input_data[0]).calc()
-        for student in rating.keys():
-            rating_score = rating[student]
-            assert pytest.approx(rating_score,
-                                 abs=0.001) == input_data[1][student]
+        rating = QuartileRating(input_data[0]).Quartile()
+        students_Q1 = rating.Quartile()
+        assert students_Q1 == input_data[1]
