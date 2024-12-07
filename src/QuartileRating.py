babc4c6cf39ad6668d0ed596df70a95bb5c9a6ae
Tsarenko Vladislav Alexandrovich <vloidex@gmail.com>
2024-12-07 14:23:48 +0300

    forming QuartileRating.py

--- src/QuartileRating.py
+++ src/QuartileRating.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-from Types import DataType
+from CalcRating import CalcRating
 
 RatingType = dict[str, float]
 
 
-class CalcRating:
-    def __init__(self, data: DataType) -> None:
-        self.data: DataType = data
+class QuartileRating:
+    def __init__(self, data: RatingType) -> None:
         self.rating: RatingType = {}
 
-    def calc(self) -> RatingType:
-        for key in self.data:
-            self.rating[key] = 0.0
-            for subject in self.data[key]:
-                self.rating[key] += subject[1]
-            self.rating[key] /= len(self.data[key])
+    def Quartile(self) -> RatingType:
+
         return self.rating
